72c223c5f431abf9d509561bfb2e02189dd0e744
huaying <royal3501@gmail.com>
2020-02-24 02:31:16 +0800

    return useful json data

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,7 @@ const app = express();
 app.get("/data", async (req, res) => {
   const balance = await bitfinext.getBalance();
   const lending = await bitfinext.getCurrentLending();
-  console.log("balance", balance);
-  console.log("lending", lending);
-  return res.status(200).json({ data: "hello" });
+  return res.status(200).json({ balance, lending });
 });
 
 exports.api = functions.https.onRequest(app);
