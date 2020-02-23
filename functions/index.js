1b24fd7206bed22499876aac0eec7c96db1c2833
huaying <royal3501@gmail.com>
2020-02-24 02:09:23 +0800

    use express in functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require("firebase-functions");
+const express = require("express");
+
 const bitfinext = require("./bitfinex");
 
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
+const app = express();
+
+app.get("/data", async (req, res) => {
   const balance = await bitfinext.getBalance();
+  const lending = await bitfinext.getCurrentLending();
   console.log("balance", balance);
-  response.send("123");
+  console.log("lending", lending);
+  return res.status(200).json({ data: "hello" });
 });
+
+exports.api = functions.https.onRequest(app);
