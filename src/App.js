ff79d6586af0937b90fbd7a3ca2b8457fd8dd37a
htsai01 <huaying.tsai@verizonmedia.com>
2020-02-24 13:14:26 +0800

    setup baseui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider, useStyletron} from 'baseui';
+
+const engine = new Styletron();
 
 function App() {
-  return <div>Hello World</div>;
+  const [css] = useStyletron();
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <div className={
+          css({
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%'}
+          )}
+        >
+          hi
+        </div>
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
 export default App;
