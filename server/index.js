ca9ad4c83e3858e19ec0e0823d1738ed906abc93
htsai01 <huaying.tsai@verizonmedia.com>
2020-02-24 12:17:29 +0800

    replace firebase functions entry with express entry

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-const functions = require("firebase-functions");
 const express = require("express");
-
 const bitfinext = require("./bitfinex");
 
 const app = express();
+const port = 3001;
 
-app.get("/data", async (req, res) => {
+app.get("/api/data", async (req, res) => {
   const balance = await bitfinext.getBalance();
   const lending = await bitfinext.getCurrentLending();
   return res.status(200).json({ balance, lending });
 });
 
-exports.api = functions.https.onRequest(app);
+app.listen(port, () => console.log(`bitfinex lending bot api on port ${port}!`))
